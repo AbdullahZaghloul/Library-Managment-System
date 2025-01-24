673a590a38bb556654ba23445e48a4e7b16158d5
Abdullah Zaghloul <abdullahzaghloul022@gmail.com>
2025-01-24 19:58:54 +0200

    first commit

---
+++ Book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Managment_System
+{
+    internal class Book
+    {
+        string title;
+        string author;
+        string ISBN;
+        bool availability;
+
+        public Book(string title,string author,string ISBN)
+        {
+            this.title = title;
+            this.author = author;
+            this.ISBN = ISBN;
+            availability = true;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+        public string GetAuthor()
+        {
+            return author;
+        }
+        public void SetAuthor(string author)
+        {
+            this.author = author;
+        }
+        public string GetISBN()
+        {
+            return ISBN;
+        }
+        public void SetISBN(string ISBN)
+        {
+            this.ISBN = ISBN;
+        }
+
+        public bool GetAvailability()
+        {
+            return availability;
+        }
+        public void SetAvailability(bool availability)
+        { 
+            this.availability = availability;
+        }
+
+
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,72 @@
+﻿namespace Library_Managment_System
+{
+    internal class Library
+    {
+        private List<Book> books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+
+        }
+
+        public bool SearchBook(string titleOrAuthor)
+        {
+            bool found = false;
+
+            foreach (Book book in books)
+            {
+                if (book.GetTitle().ToLower().Contains(titleOrAuthor.ToLower())
+                || book.GetAuthor().ToLower().Contains(titleOrAuthor.ToLower()))
+                    found = true;
+            }
+            return found;
+        }
+
+        public void BorrowBook(string titleOrAuthor)
+        {
+            bool searchResult = SearchBook(titleOrAuthor); //check if the book is exist or not
+            if (searchResult)
+            {
+                foreach (Book book in books) // get this book and set it is availability to false
+                {
+                    if (book.GetTitle().ToLower().Contains(titleOrAuthor.ToLower())
+                    || book.GetAuthor().ToLower().Contains(titleOrAuthor.ToLower()))
+                    {
+                        book.SetAvailability(false);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("This book is not in the library");
+            }
+        }
+        public void ReturnBook(string titleOrAuthor)
+        {
+            bool searchResult = SearchBook(titleOrAuthor); //check if the book is exist or not
+            if (searchResult)
+            {
+                foreach (Book book in books) // get this book and check it is availability if false set it true again
+                {
+                    if (book.GetTitle().ToLower().Contains(titleOrAuthor.ToLower())
+                    || book.GetAuthor().ToLower().Contains(titleOrAuthor.ToLower()))
+                    {
+                        if (book.GetAvailability() == false)
+                            book.SetAvailability(true);
+                        else
+                            Console.WriteLine("This book is not borrowed");
+                        break;
+                    }
+
+                }
+            }
+            else
+                Console.WriteLine("This book is not borrowed");
+
+        }
+
+
+    }
+}
